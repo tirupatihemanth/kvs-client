bb0e73239d20f1403056305c53c7e2b89c748677
HEMANTH KUMAR TIRUPATI <tirupatihemanthkumar@gmail.com>
2023-11-09 18:34:08 -0600

    kvs-client

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tirupatihemanth/kvs-client
+
+go 1.21.1
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+)
+
+var URL string = "http://localhost:80"
+var NUM_REQ int
+var NUM_CLIENTS int
+
+var outchan chan float64 = make(chan float64)
+
+func worker1(worker_id int) {
+	client := http.Client{}
+	var duration time.Duration
+	for i := 1; i <= NUM_REQ; i++ {
+		req, _ := http.NewRequest(http.MethodPut, URL, nil)
+		req.Header.Add("Key", fmt.Sprintf("Key-%d-%d", worker_id, i))
+		req.Header.Add("Val", fmt.Sprintf("Val-%d-%d", worker_id, i))
+
+		start := time.Now()
+		_, err := client.Do(req)
+		duration += time.Since(start)
+
+		if err != nil {
+			fmt.Println("Error sending request")
+		}
+
+		req, _ = http.NewRequest(http.MethodGet, URL, nil)
+		req.Header.Add("Key", fmt.Sprintf("Key-%d-%d", worker_id, i))
+
+		start = time.Now()
+		_, err = client.Do(req)
+		duration += time.Since(start)
+		if err != nil {
+			fmt.Println("Error sending request")
+		}
+	}
+
+	outchan <- float64(duration.Milliseconds())
+}
+
+func main() {
+
+	NUM_REQ, _ = strconv.Atoi(os.Args[1])
+	NUM_CLIENTS, _ = strconv.Atoi(os.Args[2])
+	latency := 0.0
+
+	start := time.Now()
+	for id := 1; id <= NUM_CLIENTS; id++ {
+		go worker1(id)
+	}
+
+	for id := 1; id <= NUM_CLIENTS; id++ {
+		latency += <-outchan
+	}
+
+	duration := time.Since(start)
+	close(outchan)
+	TOTAL_REQ := NUM_CLIENTS * NUM_REQ * 2
+	fmt.Printf("Total Requests: %d\n", TOTAL_REQ)
+	fmt.Printf("Total throughput: %f req/s\n", float64(TOTAL_REQ)/duration.Seconds())
+	fmt.Printf("Total latency: %f ms/req\n", latency/float64(NUM_CLIENTS))
+}
