9bc16f65a0f0c21a8d3e72ef77b4b82ff2a02d4c
HEMANTH KUMAR TIRUPATI <tirupatihemanthkumar@gmail.com>
2023-11-09 20:49:19 -0600

    bash

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# First time you run this the first command might time more than 10 seconds. So you ignore first run. 
 $(docker-compose -f ../kvs/docker-compose.yml up > server_logs.txt) &
 sleep 10
 echo "Sending Requests"
